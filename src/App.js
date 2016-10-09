b871244b7a1ca4e7df9cc3157f3649101dafcea9
Asad Dar <asad7dar@gmail.com>
2016-10-08 23:48:19 -0400

    fixed gifs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-import ReactCountdownClock from 'react-countdown-clock';
-var request = require('request');
-var axios = require('axios');
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
 
 class App extends Component {
   constructor() {
@@ -16,46 +12,57 @@ class App extends Component {
       currentGif: ""
     };
   }
-
+  componentDidMount(){
+      let giphyRequestUrl = 'http://api.giphy.com/v1/gifs/search?q=' + this.state.answers[this.state.placeholder] + '&api_key=dc6zaTOxFJmzC';
+     axios.get(giphyRequestUrl)
+        .then((response) => {
+          this.setState({currentGif: response.data.data[0].images.original.url});
+          debugger;
+          console.log(this.state.currentGif);
+        })
+        .catch((error) => {
+          console.log(error);
+      });;
+        debugger;
+  }
+  componentWillMount(){
+    debugger;
+  }
   handleChange(e) {
     let typing = e.target.value;
-
+    debugger;
     let place = this.state.placeholder;
     let correctAnswer = this.state.answers[place];
     
     if(typing === correctAnswer){
       this.setState({score: this.state.score + 1});
+      let tempPlaceholder = this.state.placeholder + 1;
       this.setState({placeholder: this.state.placeholder + 1});
-
-      let giphyRequestUrl = 'http://api.giphy.com/v1/gifs/search?q=' + this.state.answers[this.state.placeholder] + '&api_key=dc6zaTOxFJmzC';
+      let giphyRequestUrl = 'http://api.giphy.com/v1/gifs/search?q=' + this.state.answers[tempPlaceholder] + '&api_key=dc6zaTOxFJmzC';
+      debugger;
     axios.get(giphyRequestUrl)
-        .then(function (response) {
+        .then((response) => {
           this.setState({currentGif: response.data.data[0].images.original.url});
+          debugger;
           console.log(this.state.currentGif);
         })
-        .catch(function (error) {
+        .catch((error) => {
           console.log(error);
       });;
     }
-
   }
-
-
   render() {
     let place = this.state.placeholder;
     let idiom = this.state.idiomList[place].split("_");
-
+    debugger;
     return (
       <div className="App">
-        <h2>Gifiom</h2>
-
+        <h2>Gifioms</h2>
         <span>{idiom[0]}</span> <input onChange={this.handleChange.bind(this)} /> <span>{idiom[1]}</span>
         <img src={this.state.currentGif} alt="should've been a gif" />
         <h1> {this.state.score} </h1>
-
       </div>
     );
   }
 }
-
 export default App;
